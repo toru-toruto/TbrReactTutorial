8bf778f77e8b3c1821fa63ab446ba9ba97184f3c
toru-toruto <toruto.torutoru.toruto@gmail.com>
2026-04-16 22:02:40 +0900

    feat: use hooks

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,5 +1,6 @@
-import { useNavigate } from 'react-router-dom'
-import styles from './UserCard.module.css'
+import { useNavigate } from "react-router-dom";
+import styles from "./UserCard.module.css";
+import { useState, useRef } from "react";
 
 type UserCardProps = {
   name: string;
@@ -7,15 +8,42 @@ type UserCardProps = {
 };
 
 export function UserCard({ name, role }: UserCardProps) {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  const [isCount10, setIsCount10] = useState(false);
+
+  const count = useRef(0);
+  const nameRef = useRef<HTMLParagraphElement>(null);
+
+  const handleClick = () => {
+    count.current++;
+    if (count.current >= 10) {
+      setIsCount10(true);
+    }
+
+    if (nameRef.current) {
+      nameRef.current.style.color = "red";
+    }
+  };
 
   return (
     <div className={styles.card}>
-      <p className={styles.cardName}>{name}</p>
+      <p className={styles.cardName} ref={nameRef}>
+        {name}
+      </p>
       <p className={styles.cardRole}>{role}</p>
-      <button className={styles.cardButton} onClick={() => navigate('/profile')}>
+      <button
+        className={styles.cardButton}
+        onClick={() => navigate("/profile")}
+      >
         プロフィールを見る
       </button>
+      <button className={styles.cardButton} onClick={handleClick}>
+        カウントアップ
+      </button>
+      {isCount10 &&
+        <p className={styles.cardName}>10回押されました！</p>
+      }
     </div>
-  )
+  );
 }
--- src/pages/UserCardPage.tsx
+++ src/pages/UserCardPage.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useRef, useState } from 'react'
 import { UserCard } from '../components/UserCard'
+import styles from './UserCardPage.module.css'
 
 export function UserCardPage() {
+  const [isResized, setIsResized] = useState(false)
+  const timerRef = useRef<number>(null)
+
+  useEffect(() => {
+    const showResized = () => {
+      setIsResized(true)
+      if (timerRef.current) {
+        clearTimeout(timerRef.current)
+      }
+      timerRef.current = setTimeout(() => {
+        setIsResized(false)
+      }, 500)
+    }
+    window.addEventListener('resize', showResized)
+
+    return () => window.removeEventListener('resize', showResized)
+  }, [])
+
   return (
     <div>
       <UserCard name="田中 一郎" role="フロントエンドエンジニア" />
       <UserCard name="佐藤 太郎" role="バックエンドエンジニア" />
+      {isResized && <p className={styles.resized}>リサイズされました！</p>}
     </div>
   )
 }
